ac199c3453a8062e6d9a5877fe41cf762276c89c
DCC <github@live.hk>
2023-11-25 02:05:44 +0800

    LTSC 2021

---
+++ LTSC64/install_ms-store.ps1
@@ -0,0 +1,7 @@
+Add-AppxPackage *.Appx
+Add-AppxPackage *.AppxBundle
+Add-AppxPackage *.Msixbundle
+Write-Output "==========================================="
+Write-Output "    The Microsoft Store was installed      "
+Write-Output "==========================================="
+pause
